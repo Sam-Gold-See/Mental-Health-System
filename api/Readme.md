b74a604d22a3c3373ed57b5a4483539563c276fa
Sam Gold See <chunxin.huang@m.scnu.edu.cn>
2024-09-29 21:54:29 +0800

    实现人脸情绪识别功能

---
+++ api/Readme.md
@@ -0,0 +1,4 @@
+# 依赖库：
+pip install deepface
+opencv-python
+python版本：3.11.4（建议使用3.10版本，3.11版本有些库不行，删了降级才行）
---
+++ api/main.py
@@ -0,0 +1,4 @@
+import Emotion
+e=Emotion.Emotion()
+print(e.Get_emotion_by_picture('Test.jpg'))
+e.Get_emotion_by_video('Test.mp4')
